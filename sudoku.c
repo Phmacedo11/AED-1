5e63bf11b010a2de08c080e7824e3d5d3c09537a
Phmacedo11 <143973993+Phmacedo11@users.noreply.github.com>
2024-10-28 15:44:42 -0300

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -11,7 +11,12 @@ void le_matriz(int matriz[9][9]) {
 int confere_matriz_maior(int matriz[9][9]) {
     for (int i = 0; i < 9; i++) {
         int linha[10];  
-        int coluna[10]; 
+        int coluna[10];
+        
+        for(int s = 0; s<10; s++){
+            linha[s] = 0;
+            coluna[s] = 0;
+        }
 
         for (int j = 0; j < 9; j++) {
             int valor_linha = matriz[i][j];
@@ -41,6 +46,11 @@ int confere_matrizes_menores(int matriz[9][9]) {
     for (int i = 0; i < 9; i += 3) {
         for (int j = 0; j < 9; j += 3) {
             int submatriz[10];  
+            
+            for(int s = 0; s<10; s++){
+                submatriz[s] = 0;
+                
+            }
 
             for (int k = i; k < i + 3; k++) {
                 for (int l = j; l < j + 3; l++){
@@ -71,10 +81,11 @@ int main() {
 
         printf("Instancia %d\n", i + 1);
         if (matriz_maior && matriz_menor) {
-            printf("SIM\n");
+            printf("SIM\n\n");
         } else {
-            printf("NAO\n");
+            printf("NAO\n\n");
         }
+        
     }
     
 }
