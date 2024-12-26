67162ea8a72edc09308e976043dc6c8f69d221e5
Phmacedo11 <143973993+Phmacedo11@users.noreply.github.com>
2024-12-26 12:13:46 -0300

    Add files via upload

---
+++ Salary.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int n, k;
+    float f;
+
+    scanf("%d", &n);
+    scanf("%d", &k);
+    scanf("%f", &f);
+
+    printf("NUMBER = %d\n\n", n);
+    
+    printf("SALARY = U$ %.2f", k*f);
+
+
+}
